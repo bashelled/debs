74fd37f2f65d5c68db9f481c02498c01901d76ca
bashelled <senskeleton@aol.com>
2021-03-28 15:50:06 +0100

    reedit ppa
    	modified:   InRelease
    	modified:   KEY.gpg
    	modified:   Packages
    	modified:   Packages.gz
    	modified:   Release
    	modified:   Release.gpg
    	modified:   key.asc
    	new file:   update.sh
    	new file:   yazt-termux.deb

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -v
+KEYNAME=bashelled@gh
+(
+    set -e
+    set -v
+
+    # Packages & Packages.gz
+    dpkg-scanpackages --multiversion . > Packages
+    gzip -k -f Packages
+
+    # Release, Release.gpg & InRelease
+    apt-ftparchive release . > Release
+    gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
+    gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
+)
